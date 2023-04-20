2eb653b28d2ffc6dbcb26ed4b940430829048f0d
Harry Rich <hrich@yext.com>
2023-04-20 17:04:02 -0400

    plugin updates

--- utils.ts
+++ utils.ts
@@ -1,5 +1,5 @@
 // create app in developer console and put api key here
-const apiKey = "your_api_key";
+const API_KEY = "${{apiKey}}"
 
 // reusable function that will apply a specified template to a specified entity
 export async function applyTemplate(templateId: string, entityId: string) {
@@ -14,7 +14,7 @@ export async function applyTemplate(templateId: string, entityId: string) {
       body: requestBody
     };
 
-    await fetch(`https://api.yext.com/v2/accounts/me/entities/${entityId}?api_key=${apiKey}&v=20220613&templateId=${templateId}`, requestOptions)
+    await fetch(`https://sbx-api.yextapis.com/v2/accounts/me/entities/${entityId}?api_key=${API_KEY}&v=20230413&templateId=${templateId}`, requestOptions)
       .then(response => response.text())
       .then(result => console.log(result))
       .catch(error => console.log('error', error));
